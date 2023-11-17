cdd7a05df15a3102f2ee5360ffa8e7c036595de9
Anthony Thomas <95666367+anthonythom@users.noreply.github.com>
2023-11-16 22:07:32 -0300

    criando a estrutura do projeto

---
+++ src/main/java/br/com/afroglow/backendAfroGlow/BackendAfroGlowApplication.java
@@ -0,0 +1,13 @@
+package br.com.afroglow.backendAfroGlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAfroGlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAfroGlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/afroglow/backendAfroGlow/BackendAfroGlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.afroglow.backendAfroGlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAfroGlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
