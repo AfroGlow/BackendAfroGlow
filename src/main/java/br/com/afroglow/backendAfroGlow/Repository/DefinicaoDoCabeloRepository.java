7fd692f1da29e8c5bd32cedba5e5b7dd224a132a
Anthony Thomas <95666367+anthonythom@users.noreply.github.com>
2023-12-07 10:21:46 -0300

    att

--- src/main/java/br/com/afroglow/backendAfroGlow/Repository/DefinicaoDoCabeloRepository.java
+++ src/main/java/br/com/afroglow/backendAfroGlow/Repository/DefinicaoDoCabeloRepository.java
@@ -1,10 +1,17 @@
 package br.com.afroglow.backendAfroGlow.Repository;
 
 import br.com.afroglow.backendAfroGlow.Models.DefinicaoDoCabelo;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DefinicaoDoCabeloRepository extends JpaRepository<DefinicaoDoCabelo, Long> {
-    // Se necessário, você pode adicionar métodos personalizados aqui
+public interface DefinicaoDoCabeloRepository extends JpaRepository<DefinicaoDoCabelo, String> {
+
+    Optional<DefinicaoDoCabelo> findById(Long idDefinicaoDoCabelo);
+
+    void deleteById(Long idDefinicaoDoCabelo);
+  
 }
--- src/main/java/br/com/afroglow/backendAfroGlow/Repository/EnderecoRepository.java
+++ src/main/java/br/com/afroglow/backendAfroGlow/Repository/EnderecoRepository.java
@@ -1,10 +1,17 @@
 package br.com.afroglow.backendAfroGlow.Repository;
 
 import br.com.afroglow.backendAfroGlow.Models.Endereco;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
-    // Se necessário, você pode adicionar métodos personalizados aqui
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
+
+    Optional<Endereco> findById(Long id);
+
+    void deleteById(Long id);
+
 }
